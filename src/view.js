67dd26727e7d421353b2ce244469df3178ad0652
Elena Ozerova <ozerova.lenka@gmail.com>
2023-03-13 20:38:54 +0100

    step 2

--- src/view.js
+++ src/view.js
@@ -1,35 +1,37 @@
+/* eslint-disable no-return-assign */
 /* eslint-disable import/no-extraneous-dependencies */
+import { string } from 'yup';
 import onChange from 'on-change';
+import render from './render.js';
 
-export default (state, elements) => {
-  const watchedState = onChange(state, (path) => {
-    if (path === 'error') {
-      elements.feedback.classList.add('error');
-    } else {
-      elements.feedback.classList.remove('error');
-    }
-    elements.feedback.textContent = state.error;
-    if (state.error) {
-      elements.urlInput.classList.add('invalid');
-    } else {
-      elements.urlInput.classList.remove('invalid');
-    }
-  });
+export default (i18nInstance, elements) => {
+  const watchedState = onChange(
+    {
+      form: {
+        url: null,
+        error: {},
+      },
+      urls: [],
+    },
+    render(elements, i18nInstance),
+  );
 
   elements.rssForm.addEventListener('submit', (e) => {
-    console.log('e', e);
     e.preventDefault();
     const formData = new FormData(e.target);
-    console.log('formData ', formData);
-    const url = formData.get('url');
-    watchedState.urlInput = elements.urlInput;
-    console.log('watchedState.urlInput ', watchedState.urlInput);
-    watchedState.feedback = elements.feedback;
-    console.log('watchedState.feedback', watchedState.feedback);
-    watchedState.error = null;
-    watchedState.addFeed(url);
-    console.log('url ', url);
-  });
+    const url = formData.get('url').trim();
 
-  console.log('view is working');
+    watchedState.form.url = url;
+
+    const schema = string().url().notOneOf(watchedState.urls);
+
+    schema
+      .validate(watchedState.form.url)
+      .then(() => {
+        watchedState.urls.push(watchedState.form.url);
+        watchedState.form.url = null;
+      })
+      .catch((error) => (watchedState.form.error = error))
+      .finally(() => elements.urlInput.focus());
+  });
 };
